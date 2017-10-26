03120901ec2c0bbf7d6d1576b34a65201c95c5c8
AlexandreChikhaoui <32564249+AlexandreChikhaoui@users.noreply.github.com>
2017-10-26 10:55:08 +0200

    Add files via upload

---
+++ script.php
@@ -0,0 +1,45 @@
+<?php
+
+$compt=0;
+
+if($_GET["hped"] == "hped"){
+echo "la premiere reponse est juste";
+}
+else{
+echo "c'est faux <br>";
+}
+
+if($_GET["b"] == "ecosse"){
+echo "la deuxieme reponse est juste";
+}
+else{
+echo "c'est faux <br>";
+}
+
+if($_GET["b"] == "serdaigle"){
+echo "la troisieme reponse est juste";
+}
+else{
+echo "c'est faux <br>";
+}
+
+if($_GET["c"] == "Ginny Weasley"){
+echo "la deuxieme reponse est juste";
+}
+else{
+echo "c'est faux <br>";
+}
+
+if($_GET["c"] == "Ginny Weasley"){
+echo "la deuxieme reponse est juste";
+}
+else{
+echo "c'est faux <br>";
+}
+
+if($_GET["c"] == "Ginny Weasley"){
+echo "la deuxieme reponse est juste";
+}
+else{
+echo "c'est faux <br>";
+}
